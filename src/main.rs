60db72012a89e51197514811757a38ff1a520be6
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-04-04 23:51:05 +0500

    import crates

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
-use tui::text::Span;
-use tui::widgets::{Block, Borders, BorderType, List, ListItem, ListState, Paragraph};
+
+// crates
+use crossterm::{
+    event::{self,
+        KeyCode,
+        EnableMouseCapture,
+        DisableMouseCapture,
+        Event::Key
+    },
+    execute,
+    terminal::{
+        enable_raw_mode,
+        disable_raw_mode,
+        EnterAlternateScreen,
+        LeaveAlternateScreen},
+    
+};
+use tui::{
+    Terminal,
+    Frame,
+    widgets::{
+        Block,
+        Borders,
+        BorderType,
+        List,
+        ListItem,
+        ListState,
+        Paragraph,
+    },
+    backend::{
+        CrosstermBackend,
+        Backend
+    }
+    
+};
 
 
 
