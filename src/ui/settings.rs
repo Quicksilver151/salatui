3d9cfa6d872fc8caa0c5171c9611ceb4892da056
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-04-10 21:27:53 +0500

    Set better defaults

--- src/ui/settings.rs
+++ src/ui/settings.rs
@@ -4,19 +4,19 @@ use crate::*;
 pub fn get_settings_layout<B: Backend>(f: &mut Frame<B>) -> Vec<Rect> {
     let mut container_size : Vec<f64> = vec![];
     
-    container_size.append(&mut vec![100.0]);
-    container_size.append(&mut vec![100.0]);
-    container_size.append(&mut vec![100.0]);
-    container_size.append(&mut vec![100.0]);
+    container_size.append(&mut vec![2.0]);
+    container_size.append(&mut vec![2.0]);
+    container_size.append(&mut vec![1.0]);
+    container_size.append(&mut vec![3.0]);
     
     let total = container_size[0]/100.0 + container_size[1]/100.0 + container_size[2]/100.0 + container_size[3]/100.0;
     
-    let containers = container_size.iter().map(|x| (x/total) as u16 ).collect::<Vec<u16>>();
+    let containers : Vec<u16> = container_size.iter().map(|x| (x/total) as u16 ).collect::<Vec<u16>>();
     
     
     // dbg!(total);
     // dbg!(&containers);
-
+    
     Layout::default()
         .direction(Direction::Horizontal) .constraints([
             Constraint::Percentage(containers[0]),
