33c23043761735d5b98924bdfa22cbc993ccdcbc
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-04-24 23:07:08 +0500

    added more inputs

--- src/structs.rs
+++ src/structs.rs
@@ -5,10 +5,15 @@ pub struct InputMap {
     pub rename: bool,
     pub quit: bool,
     pub shrink: bool,
+    pub next: bool,
+    pub prev: bool,
+    pub enter: bool,
+    pub back: bool,
+
 }
 impl InputMap {
     pub fn new() -> InputMap {
-        InputMap { rename: false, quit: false, shrink: false}
+        InputMap { rename: false, quit: false, shrink: false, next: false, prev: false, enter: false, back: false}
     }
     pub fn reset(&mut self) {
         self.rename = false;
