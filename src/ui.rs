d10d6775fdddea5899dc9b5ac48837b9ea6efdd1
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-08-21 10:10:53 +0500

    finally got the planned layout

--- src/ui.rs
+++ src/ui.rs
@@ -12,22 +12,28 @@ pub fn ui<B: Backend>(f: &mut Frame<B>, app_state: &AppState){
     
     let input_map = app_state.input_map.to_owned();
     
-    fn new_block(title: &str) -> Block{
+    fn new_block(title: &str) -> Block {
         let block = Block::default()
             .title(title)
             .borders(Borders::ALL)
-            .border_type(BorderType::Rounded);
+            .border_type(BorderType::Plain);
         block
     }
-    
-    let layouts = MainLayout::from(f, &app_state);
+    fn new_block_no_outline(title: &str) -> Block {
+        let block = Block::default()
+            .title(title)
+            .borders(Borders::NONE);
+        block
+    }
+    let root_container:RootContainer = RootContainer::new(f);
+    let layouts = MainContainer::from(root_container.center);
     let title = input_map.get_current().unwrap_or(String::new());
-    let menu_block = new_block(&title);
+    let menu_block = new_block_no_outline(&title);
     
-    let block_1 = new_block("1");
-    let block_2 = new_block("2");
-    let block_3 = new_block("3");
-    let block_4 = new_block("4");
+    let block_1 = new_block("info");
+    let block_2 = new_block("");
+    let block_3 = new_block("main");
+    let block_4 = new_block("commands");
     
     let titles = ["Tab1", "Tab2", "Tab3", "Tab4"].iter().cloned().map(Spans::from).collect();
     
@@ -43,52 +49,66 @@ pub fn ui<B: Backend>(f: &mut Frame<B>, app_state: &AppState){
     // f.render_widget(block_4, layouts.settings[3]);
     // 
     // f.render_widget(tabthings, layouts.menu[0]);
-    // 
-    f.render_widget(block_1, layouts.title);
+    f.render_widget(block_1, root_container.header);
+    f.render_widget(block_2, layouts.title);
     f.render_widget(menu_block, layouts.salat);
+    f.render_widget(block_3, root_container.center);
+    f.render_widget(block_4, root_container.footer);
 }
 
-pub struct SettingsLayout {
+pub struct SettingsContainer {
     
 }
-pub struct MainLayout {
+pub struct MainContainer {
     title: Rect,
     salat: Rect,
 }
-pub struct HeaderLayout {
+pub struct HeaderContainer {
 
 }
-pub struct FooterLayout {
+pub struct FooterContainer {
 
 }
-pub enum CenterLayout{
-    Main(MainLayout),
-    Settings(SettingsLayout),
+pub enum CenterContainer{
+    Main(MainContainer),
+    Settings(SettingsContainer),
 }
-pub struct RootLayout {
-    pub header: HeaderLayout,
-    pub center: CenterLayout,
-    pub footer: FooterLayout,
+#[derive(Debug, Default)]
+pub struct RootContainer {
+    pub header: Rect,
+    pub center: Rect,
+    pub footer: Rect,
+}
+impl RootContainer {
+    fn new<B:Backend>(f: &mut Frame<B>) -> Self {
+        let layouts = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Length(3),
+            Constraint::Min(16),
+            Constraint::Length(3),
+        ]).split(f.size());
+        
+        RootContainer { header: layouts[0], center: layouts[1], footer: layouts[2] }
+    }
 }
 
 // changable setting ui + struct needed here Option{SettingType}; settingtype::choice(vec), string(or int)
-impl MainLayout {
+impl MainContainer {
     /// the main layout
-    pub fn from<B: Backend>(f: &mut Frame<B>, app_state: &AppState) -> MainLayout {
-        
-        let menu: Vec<Rect> = get_menu_layout(f);     
+    pub fn from(area: Rect) -> MainContainer {
         
         let layouts = Layout::default()
-        .direction(Direction::Vertical) .constraints([
-            Constraint::Percentage(25),
-            Constraint::Percentage(75),
-        ].as_ref()
-        ).split(f.size());
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Length(3),
+            Constraint::Min(16),
+        ]).split(area);
         
         let salat = layouts[1];
         let title = layouts[0];
         
-        MainLayout { title , salat }
+        MainContainer { title , salat }
     }
 }
 
